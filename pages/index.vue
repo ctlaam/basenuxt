4c19cf17af6a4e47d5260c2ec75518df5a2a8564
your_name <your_email@abc.example>
2025-03-11 09:48:02 +0700

    update

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+const handleMessage = () => {
+  message.info("This is a normal message");
+};
+</script>
+<template>
+  <a-button @click="handleMessage"> button </a-button>
+</template>
